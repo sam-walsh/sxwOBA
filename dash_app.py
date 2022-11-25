2ffd21e6a6bd994aa2597c644507a8c7fd7671e5
sam-walsh <64506503+sam-walsh@users.noreply.github.com>
2022-11-24 23:40:47 -0800

    changes to plots and inclusion of pulled barrels

--- dash_app.py
+++ dash_app.py
@@ -9,6 +9,7 @@
 ## -[] update font and theme
 ## -[X] fix player pictures
 ## -[X] add more player-specific plots
+## -[] add pulled barrels and % to leaderboard
 
 import plotly.express as px
 import plotly.graph_objects as go
@@ -22,14 +23,22 @@ import requests
 
 
 app = Dash(__name__)
+theme = {
+    'dark': True,
+    'detail': '#007439',
+    'primary': '#00EA64',
+    'secondary': '#6E6E6E',
+}
 server = app.server
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 df = pd.read_csv("spray_xwoba.csv")
 bbe = pd.read_csv("bbe.csv")
+bbe['field_x'] = bbe.field_x.mul(2)
+bbe['field_y'] = bbe.field_y.mul(2)
 df = df.drop(['Unnamed: 0'], axis=1)
-df= df[['batter_name', 'PA', 'xwOBA', 'sxwOBA', 'Barrels', 'diff', 'diff %', 'BB%', 'K%']]
+df= df[['batter_name', 'PA', 'wOBA', 'xwOBA', 'sxwOBA', 'Barrels', 'diff', 'diff %', 'BB%', 'K%', 'pulled_barrels']]
 df['BB%'] = df['BB%'].mul(100)
 df['K%'] = df['K%'].mul(100)
 print(df.info)
@@ -38,13 +47,10 @@ names = df.batter_name.unique().tolist()
 
 
 pitch_data = pd.read_csv("bbe.csv")
-pitch_data['field_x'] = pitch_data.field_x.mul(2.4)
-pitch_data['field_y'] = pitch_data.field_y.mul(2.4)
+pitch_data['field_x'] = pitch_data.field_x.mul(2)
+pitch_data['field_y'] = pitch_data.field_y.mul(2)
 
-fig = px.scatter(df, x="xwOBA", y="sxwOBA", color="diff", hover_data=["batter_name", "PA"], color_continuous_scale="balance")
-fig.add_shape(type="line",
-    x0=0.2, y0=0.2, x1=0.45, y1=0.45,
-    line=dict(color="RoyalBlue",width=3))
+fig = px.scatter(df, x="xwOBA", y="sxwOBA", color="diff", color_continuous_scale="balance", trendline="ols", hover_data=["batter_name", "PA"])
 
 field_swoba = px.density_heatmap(pitch_data,
     x='field_x',
@@ -61,11 +67,26 @@ fig.update_yaxes(
 
 field_swoba.add_hline(y=0, line_color="yellow")
 field_swoba.add_vline(x=0, line_color="yellow")
+field_swoba.add_shape(type="circle",
+    xref="x", yref="y",
+    x0=-360, y0=-360, x1=360, y1=360,
+    line_color="red",
+    line=dict(width=3, dash='dash')
+)
+field_swoba.add_shape(type="circle",
+    xref="x", yref="y",
+    x0=-330, y0=-330, x1=330, y1=330,
+    line_color="orange",
+    line=dict(width=3, dash='dash')
+)
+field_swoba.add_shape(type="circle",
+    xref="x", yref="y",
+    x0=-300, y0=-300, x1=300, y1=300,
+    line_color="yellow",
+    line=dict(width=3, dash='dash')
+)
 field_swoba.layout.coloraxis.colorbar.title = "sxwOBA"
-field_swoba.update_yaxes(
-    scaleanchor = "x",
-    scaleratio = 1,
-  )
+field_swoba.update_layout(xaxis_range=[-20,380], yaxis_range=[-20,380])
 
 field_xwoba = px.density_heatmap(pitch_data,
     x='field_x',
@@ -77,10 +98,26 @@ field_xwoba = px.density_heatmap(pitch_data,
     width=600)
 field_xwoba.add_hline(y=0, line_color="yellow")
 field_xwoba.add_vline(x=0, line_color="yellow")
+field_xwoba.add_shape(type="circle",
+    xref="x", yref="y",
+    x0=-360, y0=-360, x1=360, y1=360,
+    line_color="red",
+    line=dict(width=3, dash='dash')
+)
+field_xwoba.add_shape(type="circle",
+    xref="x", yref="y",
+    x0=-330, y0=-330, x1=330, y1=330,
+    line_color="orange",
+    line=dict(width=3, dash='dash')
+)
+field_xwoba.add_shape(type="circle",
+    xref="x", yref="y",
+    x0=-300, y0=-300, x1=300, y1=300,
+    line_color="yellow",
+    line=dict(width=3, dash='dash')
+)
 field_xwoba.layout.coloraxis.colorbar.title = "xwOBA"
-field_xwoba.update_yaxes(
-    scaleanchor = "x",
-    scaleratio = 1)
+field_xwoba.update_layout(xaxis_range=[-20,380], yaxis_range=[-20,380])
 
 PAGE_SIZE = 10
 
@@ -179,9 +216,9 @@ def update_data(chosen_rows, batter_dropval):
         df_filtered = df.loc[df['batter_name'] == batter_dropval]
         print(df_filtered)
 
-    barchart = px.bar(data_frame=df_filtered, x='batter_name', y=['xwOBA', 'sxwOBA'], barmode='group')
-
-
+    barchart = px.bar(data_frame=df_filtered, x='batter_name', y=['wOBA', 'xwOBA', 'sxwOBA'], barmode="group")
+    
+ 
     return barchart 
 
 @app.callback(
@@ -198,11 +235,126 @@ def update_player_scatter(batter_name):
         y='field_y',
         color='sxwOBA',
         size='sxwOBA',
-        color_continuous_scale='balance',
+        opacity=1,
+        color_continuous_scale='amp',
         hover_data=['launch_speed', 'launch_angle', 'pulled_barrel'],
-        width=800,
+        width=750,
         height=700)
 
+    bbe_scatter.update_traces(line=dict(dash='dash', width=3, color="white"))
+    
+    bbe_scatter.update_layout(xaxis_range=[-20,380], yaxis_range=[-20,380],
+        xaxis = dict(
+            tickmode='array',
+            tickvals = [0, 90, 300, 330, 360]
+        ),
+        yaxis = dict(
+            tickmode='array',
+            tickvals = [0, 90, 300, 330, 360]
+        )
+    )
+    
+    bbe_scatter.add_shape(type="rect",
+        x0=-20, y0=-20, x1=380, y1=380,
+        line=dict(
+            color="green",
+            width=5,
+        ),
+        opacity=0.2,
+        layer="below",
+        fillcolor="green",
+    )
+    bbe_scatter.add_shape(type="circle",
+        xref="x", yref="y",
+        x0=-360, y0=-360, x1=360, y1=360,
+        line_color="yellow",
+        opacity=0.5,
+        layer="below",
+        line=dict(width=3, dash='dash')
+    )
+    bbe_scatter.add_shape(type="circle",
+        xref="x", yref="y",
+        x0=-330, y0=-330, x1=330, y1=330,
+        line_color="yellow",
+        opacity=0.5,
+        layer="below",
+        line=dict(width=3, dash='dash')
+    )
+    bbe_scatter.add_shape(type="circle",
+        xref="x", yref="y",
+        x0=-300, y0=-300, x1=300, y1=300,
+        line_color="yellow",
+        opacity=0.5,
+        layer="below",
+        line=dict(width=3, dash='dash')
+    )
+    bbe_scatter.add_shape(type="circle",
+        xref="x", yref="y",
+        x0=-155, y0=-155, x1=155, y1=155,
+        fillcolor="white",
+        opacity=1,
+        layer="below",
+        line_color="white"
+    )
+    bbe_scatter.add_shape(type="circle",
+        xref="x", yref="y",
+        x0=-100, y0=-100, x1=160, y1=160,
+        fillcolor="#efdcc3",
+        opacity=1,
+        layer="below",
+        line_color="#efdcc3"
+    )
+    bbe_scatter.add_shape(type="rect",
+        x0=-20, y0=-20, x1=0, y1=155,
+        fillcolor="#b7d7c5",
+        layer="below",
+        opacity=1,
+        line_color="#b7d7c5"
+    )
+    bbe_scatter.add_shape(type="rect",
+        x0=-20, y0=-20, x1=155, y1=0,
+        fillcolor="#b7d7c5",
+        layer="below",
+        opacity=1,
+        line_color="#b7d7c5"
+    )
+    bbe_scatter.add_shape(type="rect",
+        x0=0, y0=0, x1=90, y1=90,
+        line=dict(
+            color="white",
+            width=5,
+        ),
+        layer="below",
+        opacity=0.8,
+    )
+    bbe_scatter.add_shape(type="rect",
+        x0=0, y0=0, x1=90, y1=90,
+        fillcolor="white",
+        layer="below",
+        opacity=1,
+        line_color="white"
+    )
+    bbe_scatter.add_shape(type="rect",
+        x0=0, y0=0, x1=90, y1=90,
+        fillcolor="#b7d7c5",
+        layer="below",
+        opacity=1,
+        line_color="white"
+    )
+    bbe_scatter.add_shape(type="circle",
+        xref="x", yref="y",
+        x0=-15, y0=-15, x1=15, y1=15,
+        fillcolor="#efdcc3",
+        opacity=1,
+        layer="below",
+        line_color="#efdcc3")
+    bbe_scatter.add_shape(type="circle",
+        xref="x", yref="y",
+        x0=50.5, y0=50.5, x1=70.5, y1=70.5,
+        fillcolor="#efdcc3",
+        opacity=1,
+        layer="below",
+        line_color="#efdcc3")
     return bbe_scatter
 
 @app.callback(
@@ -235,7 +387,7 @@ def update_styles(selected_columns):
 def update_table(selected_pa):
     filtered_df = df[df.PA >= selected_pa]
 
-    fig = px.scatter(filtered_df, x="xwOBA", y="sxwOBA", color="diff", hover_data=["batter_name", "PA", "diff %"])
+    fig = px.scatter(filtered_df, x="xwOBA", y="sxwOBA", color="diff", color_continuous_scale="balance", trendline="ols", hover_data=["batter_name", "PA", "diff %"])
 
     return fig
 
