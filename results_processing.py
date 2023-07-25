bb8f1e19e4a3fb374e6f8efe963759675e4bdf28
sam-walsh <64506503+sam-walsh@users.noreply.github.com>
2023-07-25 15:36:21 -0700

    GIDP prob model

--- results_processing.py
+++ results_processing.py
@@ -56,7 +56,7 @@ def calculate_expected_xwoba(df, bbe, year):
     woba_events.loc[:, 'xwOBA_adj'] = woba_events['estimated_woba_using_speedangle'].sub(woba_events['gidp_adj'])
     print(woba_events['sxwOBA_adj'].describe())
     if year in [2021, 2022, 2023]:
-        woba_events.to_csv(f'woba_events_{year}.csv')
+        woba_events.to_csv(f'statcast_data/woba_events_{year}.csv')
     leaders = woba_events.groupby('batter')[['rf_xwoba', 'sxwOBA', 'sxwOBA_adj']].mean().reset_index()
 
     # sxwoba_leaders = woba_events.groupby('batter')['sxwoba_prob_model'].agg(['mean', 'count'])
